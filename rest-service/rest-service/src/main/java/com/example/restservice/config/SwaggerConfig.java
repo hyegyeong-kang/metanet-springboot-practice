2731f22df444db84a2c41ad4a500c2058795630e
hyegyeong-kang <rkdgprud96@gmail.com>
2023-03-21 17:38:51 +0900

    test swagger

---
+++ rest-service/rest-service/src/main/java/com/example/restservice/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.restservice.config;
+//
+//import org.springframework.beans.factory.annotation.Configurable;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+// 컨트롤러 같은거 문서화 시키는 것
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2);
+//    }
+//}
