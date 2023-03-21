ee17eb088ddf4a8828d3c31b64343a24cd080e86
hyegyeong-kang <rkdgprud96@gmail.com>
2023-03-21 12:05:44 +0900

    test Hellocontroller, HelloWorldbean

---
+++ rest-service/rest-service/src/main/java/com/example/restservice/helloworld/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.restservice.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World Bean!!");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World Bean, %s", name));
+    }
+}
